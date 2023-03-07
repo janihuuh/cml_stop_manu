5cfd0a32a5f73c1e0b53d52fd0a463b8222c67ec
Jani Huuhtanen <jani.huuhtanen@helsinki.fi>
2023-03-07 15:24:41 +0200

    Delete run_cellphonedb.R

--- R/run_cellphonedb.R
+++
@@ -1,9 +0,0 @@
-
-## Run cellphone db. Make subselection of 1000 cells from responders at time 0 and time 1
-sample_n = 1000
-
-################################ Init cellphone input files
-
-### no relapse
-data.frame(cml_seurat@meta.data, cluster = Idents(cml_seurat)) %>%
-  initCellphonedb(seurat_object = public_seurat, name = "public_seurat", sample_n = sample_n)
